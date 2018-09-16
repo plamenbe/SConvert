574714b4cd247d99a0f449a1f490e9f380758e4f
Plamen Bekyarov <plamenbekyarov@yahoo.com>
2018-09-16 23:50:49 +0300

    Nightly build

--- src/version.h
+++ src/version.h
@@ -2,32 +2,32 @@
 #define VERSION_H
 
 namespace AutoVersion{
-	
+
 	//Date Version Types
-	#define DATE "15"
+	#define DATE "16"
 	#define MONTH "09"
 	#define YEAR "2018"
 	#define UBUNTU_VERSION_STYLE  "18.09"
-	
+
 	//Software Status
 	#define STATUS  "Beta"
 	#define STATUS_SHORT  "b"
-	
+
 	//Standard Version Type
 	#define MAJOR  1
 	#define MINOR  0
-	#define BUILD  2
-	#define REVISION  13
-	
+	#define BUILD  1
+	#define REVISION  7
+
 	//Miscellaneous Version Types
-	#define BUILDS_COUNT  11
-	#define RC_FILEVERSION 1,0,2,13
-	#define RC_FILEVERSION_STRING "1, 0, 2, 13\0"
-	#define FULLVERSION_STRING  "1.0.2.13"
-	
+	#define BUILDS_COUNT  23
+	#define RC_FILEVERSION 1,0,1,7
+	#define RC_FILEVERSION_STRING "1, 0, 1, 7\0"
+	#define FULLVERSION_STRING  "1.0.1.7"
+
 	//These values are to keep track of your versioning state, don't modify them.
-	#define BUILD_HISTORY  2
-	
+	#define BUILD_HISTORY  1
+
 
 }
 #endif //VERSION_H
